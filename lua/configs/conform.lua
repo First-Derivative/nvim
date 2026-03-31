25af07069faaebbe3f0443daf3457139aea290f6
First-Derivative <ashraff.hatz@gmail.com>
2026-03-31 12:55:03 +0100

    feat: prettier support

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,31 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    javascript = { "prettier" },
+    javascriptreact = { "prettier" },
+    typescript = { "prettier" },
+    typescriptreact = { "prettier" },
+    json = { "prettier" },
+    markdown = { "prettier" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  formatters = {
+    prettier = {
+      prepend_args = function(self, ctx)
+        if ctx.filename:match("%.tpl$") then
+          return { "--parser", "json" }
+        end
+        return {}
+      end,
+    },
+  },
+
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,12 @@ require "nvchad.options"
 
 -- add yours here!
 
+-- Treat .tpl files as JSON
+vim.filetype.add({
+  extension = {
+    tpl = "json",
+  },
+})
+
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
